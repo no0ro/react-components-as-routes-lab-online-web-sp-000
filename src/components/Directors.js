29927a89dc3b9e165e3c05e2f9d8b2b711153359
no0ro <nrolwes@gmail.com>
2020-11-30 06:22:35 -0500

    commit tree a13286e01d723524ce2a60b3a95041805cb8067c

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,8 +3,19 @@ import { directors } from '../data';
 
 const Directors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+<div>
+     
+      <h1>Directors Page</h1>
+      {directors.map((director) => 
+        <div>
+          <h3>{director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map(movie => 
+              <li>{movie}</li>)}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
